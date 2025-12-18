fd0283b590bde7efd4afadba3e69d6f58f469b0e
prathi620 <prathikutty620@gmail.com>
2025-12-18 10:27:31 +0530

    Fix pre-save hook in User model
    Added missing next() call in pre-save hook.

--- server/models/User.js
+++ server/models/User.js
@@ -18,6 +18,7 @@ userSchema.pre('save', async function (next) {
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
